393543859d775d1f6f734416410685ce75925d52
Tirth24052002 <tirth.p@simformsolutions.com>
2023-05-29 18:23:03 +0530

    - Basic Auto Layout

---
+++ autoLayout/autoLayout/WelcomePageVC.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomePageVC.swift
+//  autoLayout
+//
+//  Created by Tirth Purohit on 29/05/23.
+//
+
+import UIKit
+
+class WelcomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
